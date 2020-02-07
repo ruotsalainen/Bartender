9c8ef0caf0b4e0212f991837e5503eeb7296aecf
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-07 22:41:20 +0200

    test

--- test.py
+++ test.py
@@ -5,13 +5,28 @@ from signal import pause
 red = PWMLED(17)
 blue = PWMLED(4)
 
-while True:
-    red.value = 1
-    blue.value = 1
-    sleep(0.2)
-    red.value = 0.5
-    blue.value = 0.5
-    sleep(0.2)
-    red.value = 0
-    blue.value = 0
-    sleep(0.2)
+selection = input("valitse testi 1, 2, 3")
+if selection == 1:
+    while True:
+        red.value = 1
+        blue.value = 1
+        sleep(0.2)
+        red.value = 0.5
+        blue.value = 0.5
+        sleep(0.2)
+        red.value = 0
+        blue.value = 0
+        sleep(0.2)
+elif selection == 2:
+    while True:
+        red.blink(0.5)
+        blue.blink(0.2)
+        sleep(0.5)
+elif selection == 3:
+    while True:
+        red.on()
+        for i in range(1, 11):
+            blue.value = i/10
+        blue.on()
+        for i in range(1, 11):
+            red.value = i/10
