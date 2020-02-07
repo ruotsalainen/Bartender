e16e949c324186481be9c3b91fd9140eba223425
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-07 21:37:31 +0200

    testfile

---
+++ test.py
@@ -0,0 +1,13 @@
+from gpiozero import LED
+from time import sleep
+
+red = LED(17)
+blue = LED(4)
+
+while True:
+    red.on()
+    blue.off()
+    sleep(1)
+    red.off()
+    blue.on()
+    sleep(1)
