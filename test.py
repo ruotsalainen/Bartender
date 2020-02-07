1d75338becbbecfec663453bf7e7e3e9d1644c31
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-07 22:28:28 +0200

    test

--- test.py
+++ test.py
@@ -5,14 +5,13 @@ from signal import pause
 red = PWMLED(17)
 blue = PWMLED(4)
 
-red.value = 1
-blue.value = 1
-sleep(0.2)
-red.value = 0.5
-blue.value = 0.5
-sleep(0.2)
-red.value = 0
-blue.value = 0
-sleep(0.2)
-
-pause()
+while True:
+    red.value = 1
+    blue.value = 1
+    sleep(0.2)
+    red.value = 0.5
+    blue.value = 0.5
+    sleep(0.2)
+    red.value = 0
+    blue.value = 0
+    sleep(0.2)
