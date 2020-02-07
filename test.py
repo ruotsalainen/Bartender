c9b3e35792960613547203ae26e83cc834f346b2
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-07 22:08:03 +0200

    test

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-from gpiozero import LED
+from gpiozero import LED, PWMLED
 from time import sleep
 
 red = LED(17)
-blue = LED(4)
+blue = PWMLED(4)
 
 while True:
     red.on()
     blue.off()
-    sleep(1)
+    sleep(0.5)
     red.off()
     blue.on()
     sleep(0.5)
