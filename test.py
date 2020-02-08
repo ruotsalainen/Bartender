d3ab443169c065248ac5e602ed0b4c8215827faf
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-08 14:31:21 +0200

    lcd

---
+++ test.py
@@ -0,0 +1,17 @@
+from RPLCD.i2c import CharLCD
+from gpiozero import Button
+
+button = Button(17)
+
+lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
+              cols=20, rows=4, dotsize=8,
+              charmap='A02',
+              auto_linebreaks=True,
+              backlight_enabled=True)
+
+
+
+strings = ["Hello", "R2", "What", "would", "you", "like", "on", "your", "pizza"]
+for i in range(1, 10):
+    if button.is_active:
+        lcd.write_string(strings[i])
