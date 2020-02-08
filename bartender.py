554c883795fe30c544c6a364942624dc10fb2356
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-08 14:11:27 +0200

    lcd

--- bartender.py
+++ bartender.py
@@ -1,5 +1,9 @@
 from RPLCD.i2c import CharLCD
 
-lcd = CharLCD('PCF8574', 0x27)
+lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
+              cols=20, rows=4, dotsize=8,
+              charmap='A02',
+              auto_linebreaks=True,
+              backlight_enabled=True)
 
 lcd.write_string("Hello world")
