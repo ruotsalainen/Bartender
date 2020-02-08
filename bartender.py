f5240bf680c4f277f9786bdb923400bc4f677105
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-09 01:10:43 +0200

    menu

---
+++ bartender.py
@@ -0,0 +1,12 @@
+drink_options = [
+	{"name": "Gin", "value": "gin"},
+	{"name": "Rum", "value": "rum"},
+	{"name": "Vodka", "value": "vodka"},
+	{"name": "Tequila", "value": "tequila"},
+	{"name": "Tonic Water", "value": "tonic"},
+	{"name": "Coke", "value": "coke"},
+	{"name": "Orange Juice", "value": "oj"},
+	{"name": "Margarita Mix", "value": "mmix"}
+]
+
+print(drink_options[1].get("name"))
---
+++ pizza.py
@@ -0,0 +1,22 @@
+from RPLCD.i2c import CharLCD
+from gpiozero import Button
+from time import sleep
+
+button = Button(17)
+
+lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
+              cols=20, rows=4, dotsize=8,
+              charmap='A02',
+              auto_linebreaks=True,
+              backlight_enabled=True)
+
+
+
+strings = ["Hello", "R2", "What", "would", "you", "like", "on", "your", "pizza"]
+for i in range(9):
+    if button.wait_for_active():
+        lcd.write_string(strings[i])
+        sleep(0.5)
+        lcd.clear()
+
+lcd.backlight_enabled = False
