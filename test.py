10012c6b01a39a783ebcaca15923925f77a3b8e5
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-08 23:39:21 +0200

    menu

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ from gpiozero import Button
 from time import sleep
 import sys
 
-button = Button(17, hold_time=2)
+button_right = Button(17)
+button_left = Button(27)
 
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               cols=20, rows=4, dotsize=8,
@@ -20,16 +21,34 @@ menu_options = {
 
 current_option = 1
 
+drinks = {
+    1:"Rum&Coke"
+}
+
+current_drink = 1
+
+
 
 while True:
+    lcd.clear()
     lcd.write_string(menu_options[current_option])
     sleep(BOUNCE)
-    if button.wait_for_active():
+    if button_right.wait_for_active():
         lcd.clear()
         if current_option == 2:
             current_option -= 1
         else:
             current_option += 1
-    if button.is_held:
-        lcd.backlight_enabled = False
-        sys.exit()
+    if button_left.wait_for_active():
+        if current_option == 1:
+            lcd.clear()
+            lcd.write_string(drinks[current_drink])
+            sleep(5)
+
+        elif current_option == 2:
+            lcd.clear()
+            lcd.write_string("haha")
+
+        else:
+            lcd.backlight_enabled = False
+            sys.exit()
