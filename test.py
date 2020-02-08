89cc177b245c83f4d0436620d8c30ea93a324aca
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-08 17:38:51 +0200

    menu

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 from RPLCD.i2c import CharLCD
 from gpiozero import Button
 from time import sleep
+import sys
 
-button = Button(17)
+button = Button(17, hold_time=2)
 
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               cols=20, rows=4, dotsize=8,
               charmap='A02',
               auto_linebreaks=True,
               backlight_enabled=True)
+
+BOUNCE = 0.5
 
 menu_options = {
     1: "Tee juoma",
@@ -17,14 +20,15 @@ menu_options = {
 
 current_option = 1
 
-print(menu_options[current_option])
 
 while True:
     lcd.write_string(menu_options[current_option])
-    sleep(1)
+    sleep(BOUNCE)
     if button.wait_for_active():
         lcd.clear()
         if current_option == 2:
             current_option -= 1
         else:
             current_option += 1
+    if button.is_held:
+        sys.exit()
