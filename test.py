7ba805bca821193de639c7d2ea04c9a0bbde2075
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-09 12:39:22 +0200

    input write

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
 from RPLCD.i2c import CharLCD
-from gpiozero import Button
-from time import sleep
-import sys
-from drinks import drink_list
-from config import config_options
-
-button_select = Button(17)
-button_advance = Button(27)
-button_config = Button(22)
 
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               cols=20, rows=4, dotsize=8,
@@ -15,100 +6,6 @@ lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               auto_linebreaks=True,
               backlight_enabled=True)
 
-BOUNCE = 0.5
-
-
-
-current_drink = 0
-modified = True
-
-def next_drink():
-    global modified
-    global current_drink
-    modified = True
-
-    if current_drink == len(drink_list)-1:
-        current_drink = 0
-    else:
-        current_drink += 1
-
-def make_drink():
-    global modified
-    modified = True
-
-    lcd.write_string("Coming right up!")
-    sleep(2)
-
-def open_config():
-    global modified
-    modified = True
-    current_option = 0
-    while True:
-        if modified:
-            lcd.clear()
-            task_name = config_options[current_option].get("task")
-            whitespace = int((20-len(task_name))/2)
-            lcd.cursor_pos = (1, whitespace)
-            lcd.write_string(task_name)
-            sleep(BOUNCE)
-            modified = False
-
-        if button_advance.is_active:
-            modified = True
-            if current_option == len(config_options)-1:
-                current_option = 0
-            else:
-                current_option += 1
-
-        if button_select.is_active:
-            modified = True
-            if current_option == 0:
-                lcd.clear()
-                lcd.cursor_pos = (1, 0)
-                lcd.write_string("Flushing the system")
-                sleep(2)
-            if current_option == 1:
-                lcd.clear()
-                lcd.cursor_pos = (1, 7)
-                lcd.write_string("Ciao!")
-                sleep(2)
-                lcd.backlight_enabled = False
-                exit()
-                
-        
-        if button_config.is_active:
-            modified = True
-            break
-
-def greeting():
-    lcd.cursor_pos = (1, 7)
-    lcd.write_string("Hello!")
-    sleep(1)
-    lcd.cursor_pos = (2, 0)
-    lcd.write_string("My name is Giovanni.")
-    sleep(2)
-
-
-greeting()
-
 while True:
-    if modified:
-        lcd.clear()
-        drink_name = drink_list[current_drink].get("name")
-        whitespace = int((20-len(drink_name))/2)
-        lcd.cursor_pos = (1, whitespace)
-        lcd.write_string(drink_name)
-        sleep(BOUNCE)
-        modified = False
-    if button_select.is_active:
-        next_drink()
-
-    if button_advance.is_active:
-        lcd.cursor_pos = (2, 2)
-        make_drink()
-    
-    if button_config.is_active:
-        lcd.clear()
-        open_config()
-        modified = True
-
+    text = input("")
+    lcd.write_string(text)
