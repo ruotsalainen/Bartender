32e0ae655b3bb0d7a0f984b7ee2e8b929665b7a2
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-09 16:54:39 +0200

    input write

--- smiley.py
+++ smiley.py
@@ -1,4 +1,6 @@
 from RPLCD.i2c import CharLCD
+from time import sleep
+
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               cols=20, rows=4, dotsize=8,
               charmap='A02',
@@ -16,5 +18,24 @@ smiley = (
     0b00000,
 )
 
-lcd.create_char(0, smiley)
-lcd.write_string(chr(0))
+full = (
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111
+)
+
+
+lcd.create_char(0, full)
+lcd.create_char(1, smiley)
+
+for i in range(20):
+    lcd.write_string(chr(0))
+    sleep(0.5)
+
+lcd.write_string(chr(1))
+sleep(2)
