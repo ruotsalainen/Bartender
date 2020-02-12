d4d7997f23857a5b8b943313e235e2c7c12f6e39
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-02-12 23:39:22 +0200

    animation

--- suttu.py
+++ suttu.py
@@ -35,9 +35,20 @@ frame1,frame2,frame3 = (
 	0b10001,
 	0b11111
 )
-lcd.create_char(0, frame2)
-lcd.create_char(1, frame1)
+splash = (
+	0b00000,
+	0b00000,
+	0b00100,
+	0b00010,
+	0b00000,
+	0b01000,
+	0b00100,
+	0b00000
+)
+lcd.create_char(0, frame1)
+lcd.create_char(1, frame2)
 lcd.create_char(2, frame3)
+lcd.create_char(3, splash)
 
 try:
 	while True:
@@ -48,6 +59,10 @@ try:
 			lcd.cursor_pos = (1, 11 - i)
 			lcd.write_string(chr(max(i-2, 2-i)))
 			sleep(0.5)
+		lcd.cursor_pos = (0, 8)
+		lcd.write_string(chr(3))
+		lcd.cursor_pos = (0, 9)
+		lcd.write_string(chr(3))
 except KeyboardInterrupt:
 	lcd.backlight_enabled = False
 	lcd.close()
