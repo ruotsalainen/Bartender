d5a94a11794fff1c6fc95b15f7cca7600ece1543
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2020-07-26 22:53:45 +0300

    drinks&pumps updated for NC2020

--- pumps.py
+++ pumps.py
@@ -1,34 +1,26 @@
 pumps_list =  [
     {
         "name": "pump1",
-        "value": "rum"
-    },
-    {
-        "name": "pump2",
-        "value": "coke"
-    },
-    {
-        "name": "pump3",
-        "value": "gin"
-    },
-    {
-        "name": "pump4",
         "value": "vodka"
     },
     {
+        "name": "pump2",
+        "value": "gin"
+    },
+    {
+        "name": "pump3",
+        "value": "rum"
+    },
+    {
+        "name": "pump4",
+        "value": "myrkky"
+    },
+    {
         "name": "pump5",
-        "value": "oj"
+        "value": ""
     },
     {
         "name": "pump6",
-        "value": "sprite"
-    },
-    {
-        "name": "pump7",
-        "value": "mint liquor"
-    },
-    {
-        "name": "pump8",
-        "value": "raspberry liquor"
+        "value": ""
     }
 ]
